3df299a7f414a117ddd1c8091c5582ace33896fa
offcn-java <2293624110@qq.com>
2020-12-17 17:36:59 +0800

    2020/12.17 添加控制类 xie

--- src/main/java/com/offcn/thymeleafdemo/controller/T123.java
+++ src/main/java/com/offcn/thymeleafdemo/controller/T123.java
@@ -1,8 +1,10 @@
 package com.offcn.thymeleafdemo.controller;
 
 public class T123 {
+
     public static void main(String[] args) {
-       String a = "修改啊aaaaaaaaaaaaa17:28";
+        String a = "修改啊aaaaaaaaaaaaa17:28";
+        String a2 = "修改啊aaaaaaaaaaaaa17:33";
     }
 
 }
---
+++ src/main/java/com/offcn/thymeleafdemo/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.offcn.thymeleafdemo.controller;
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("123>>17:35");
+    }
+
+}
