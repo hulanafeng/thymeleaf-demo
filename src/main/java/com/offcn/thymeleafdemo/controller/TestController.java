d6d313abb6e67bc0cf06ced28684ae823b2260ef
offcn-java <2293624110@qq.com>
2020-12-17 17:44:20 +0800

    2020/12.17 加个输出 xie

--- src/main/java/com/offcn/thymeleafdemo/controller/TestController.java
+++ src/main/java/com/offcn/thymeleafdemo/controller/TestController.java
@@ -3,6 +3,7 @@ package com.offcn.thymeleafdemo.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("123>>17:35");
+        System.out.println(">>>>>>>>>>>>>>>");
     }
 
 }
